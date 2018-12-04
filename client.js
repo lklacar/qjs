0faedbf0bf585f7ff83575ed315ea46eed1b0db7
Luka Klacar <l.klacar@vegaitsourcing.rs>
2018-12-04 16:57:37 +0100

    Minor changes

--- client.js
+++ client.js
@@ -7,16 +7,24 @@ const ws = new WebSocket('ws://master.quakejs.com:27950/', {
   }
 });
 
+
+const messageOptions = {
+  binary: true,
+  compress: true,
+  fin: true,
+  mask: true,
+};
+
 ws.binaryType = "binary";
 ws.on('open', function (res) {
 
-  ws.send(textToBuffer("heartbeat ComeOn"), {binary: true}, null);
+  ws.send(textToBuffer("heartbeat ComeOn"), messageOptions, null);
 
   ws.on('message', function (res) {
     const view = Uint8Array.from(res);
     const buffer = view.buffer;
-
     const response = bufferToText(buffer);
+    console.log(response);
 
     if (response.indexOf('getinfo') !== -1) {
 
@@ -24,7 +32,7 @@ ws.on('open', function (res) {
       const infoResponse = "infoResponse\n" +
         "\\g_needpass\\0\\pure\\1\\gametype\\0\\sv_maxclients\\12\\g_humanplayers\\0\\clients\\0\\mapname\\q3dm7\\hostname\\CHANGE ME\\protocol\\71\\gamename\\Quake3Arena\\challenge\\" + challenge;
 
-      ws.send(textToBuffer(infoResponse), {binary: true}, null);
+      ws.send(textToBuffer(infoResponse), messageOptions, null);
 
     }
   })
